c7236e995ffafcea121b317efeefd67faeee5d03
EchoAI-blink <echoai1234@proton.me>
2024-09-16 15:26:44 +0530

    Update wifits.sh

--- wifits.sh
+++ wifits.sh
@@ -1,9 +1,9 @@
-apt install wifite
-apt install hcxtools
-apt install hcxdumptool
-apt install reaver
-apt install cowpatty
-apt install gcc
+sudo apt install wifite
+sudo apt install hcxtools
+sudo apt install hcxdumptool
+sudo apt install reaver
+sudo apt install cowpatty
+sudo apt install gcc
 git clone https://github.com/JPaulMora/Pyrit.git
 cd Pyrit
 python setup.py clean
