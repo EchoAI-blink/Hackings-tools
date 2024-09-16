51b7181dd6a49342192bc0f09dcfcaa62730923b
EchoAI-blink <echoai1234@proton.me>
2024-09-16 15:18:06 +0530

    Create wifits.sh

---
+++ wifits.sh
@@ -0,0 +1,11 @@
+apt install wifite
+apt install hcxtools
+apt install hcxdumptool
+apt install reaver
+apt install cowpatty
+apt install gcc
+git clone https://github.com/JPaulMora/Pyrit.git
+cd Pyrit
+python setup.py clean
+python setup.py build
+python setup.py install
